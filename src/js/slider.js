e055637da0af0e9c273c7d978d1039ce1e126961
Amanda Meneguini <admcmarshmallow@gmail.com>
2024-02-13 21:33:23 -0300

    implements new value to "ehAUltimaImagem"

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,81 +1,56 @@
-// const setaDireita = document.querySelector(".seta-direita")
-// const setaEsquerda = document.querySelector(".seta-esquerda")
-
-// const temaAtual = document.querySelector(".imagem-de-fundo")
-
-// const tituloPrincipal = document.querySelector(".titulo-principal")
-
-// setaDireita.addEventListener('click', () => {
-
-//     if(temaAtual.classList.contains('primeiro-tema')){
-//         temaAtual.classList.add('segundo-tema');
-//         temaAtual.classList.remove('primeiro-tema');
-
-//         setaEsquerda.classList.remove('seta-desativada')
-
-//         tituloPrincipal.innerHTML = 'Título imagem 2'
-
-//     }else if(temaAtual.classList.contains('segundo-tema')){
-//         temaAtual.classList.add('terceiro-tema');
-//         temaAtual.classList.remove('segundo-tema');
-
-//         tituloPrincipal.innerHTML = 'Título imagem 3'
-
-//     }else if(temaAtual.classList.contains('terceiro-tema')){
-//         temaAtual.classList.add('quarto-tema')
-//         temaAtual.classList.remove('terceiro-tema');
-
-//         setaDireita.classList.add('seta-desativada')
-//         setaEsquerda.classList.remove('seta-desativada')
-
-//         tituloPrincipal.innerHTML = 'Título imagem 4'
-//     }
-// })
-
-// setaEsquerda.addEventListener('click', () => {
-
-//     if(temaAtual.classList.contains('quarto-tema')){
-//         temaAtual.classList.add('terceiro-tema');
-//         temaAtual.classList.remove('quarto-tema');
-
-//         setaEsquerda.classList.remove('seta-desativada')
-//         setaDireita.classList.remove('seta-desativada')
-
-//         tituloPrincipal.innerHTML = 'Título imagem 3'
-
-//     }else if(temaAtual.classList.contains('terceiro-tema')){
-//         temaAtual.classList.add('segundo-tema')
-//         temaAtual.classList.remove('terceiro-tema');
-
-//         setaDireita.classList.remove('seta-desativada')
-
-//         tituloPrincipal.innerHTML = 'Título imagem 2'
-
-//     }else if(temaAtual.classList.contains('segundo-tema')){
-//         temaAtual.classList.add('primeiro-tema')
-//         temaAtual.classList.remove('segundo-tema');
-
-//         setaEsquerda.classList.add('seta-desativada')
-//         setaDireita.classList.remove('seta-desativada')
-
-//         tituloPrincipal.innerHTML = 'Título imagem 1'
-//     }
-// })
-
+const imagens = document.querySelectorAll(".slide")
+console.log(imagens);
 const setaAvancar = document.getElementById("seta-avancar");
 const setaVoltar = document.getElementById("seta-voltar");
 
 let imagemAtual = 0
 
 setaAvancar.addEventListener('click', function(){
-
+    if(imagemAtual === imagens.length - 1){
+        return;
+    }
     
+    imagemAtual++;
 
-    esconderImagem()
-    mostrarImagem()
-    ativarOuDesativarSeta()
-})
+    esconderImagemAberta();
+    mostrarImagem();
+    ativarOuDesativarSeta();
+});
 
-function esconderImagem(){
-    const 
-}
+setaVoltar.addEventListener('click', function(){
+    if(imagemAtual === 0){
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagemAberta();
+    mostrarImagem();
+    ativarOuDesativarSeta();
+});
+
+function esconderImagemAberta(){
+    const imagemAberta = document.querySelector(".mostrar");
+    imagemAberta.classList.remove("mostrar");
+};
+
+function mostrarImagem(){
+    imagens[imagemAtual].classList.add("mostrar");
+};
+
+function ativarOuDesativarSeta(){
+    const NaoEhAPrimeiraImagem = imagemAtual !== 0
+    if(NaoEhAPrimeiraImagem){
+        setaVoltar.classList.remove("opacidade");
+    }else{
+        setaVoltar.classList.add("opacidade");
+    };
+    
+    const ehAUltimaImagem = imagemAtual !== 0 && imagemAtual === imagens.length - 1
+    if(ehAUltimaImagem){
+        setaAvancar.classList.add("opacidade");
+    }else{
+        setaAvancar.classList.remove("opacidade");
+    };
+
+};
